5019dd3aabcae1ce987016bfa67e80a5dd74d325
murilo <murilocolucci@gmail.com>
2026-08-21 11:39:17 -0300

    arithmetics

---
+++ Arithmetic/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 22;
+        //int z;
+
+        //z = x + y;
+
+        //x++;
+
+        //x += y;
+        //x -= y;
+        //x *= y;
+        //x /= y;
+        x %= y;
+
+        System.out.println(x);
+
+        //order of operations [P-E-M-D-A-S]
+        //double result = 3 + 4 * (7 - 5) / 2.0;
+    }
+}
