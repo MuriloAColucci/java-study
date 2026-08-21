c05ad844c9116194390641594630230f9736fff1
murilo <murilocolucci@gmail.com>
2026-08-21 10:29:37 -0300

    Initial commit

---
+++ Variables/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 30;
+        int year = 2026;
+        int quantity = 1;
+
+        double price = 19.99;
+        double gpa = 3.5;
+        double temperature = -12.5;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        boolean isStudent = true;
+        boolean forSale = false;
+        boolean isOnline = true;
+
+        String name = "Murilo";
+        String food = "pizza";
+        String email = "email123@gmail.com";
+        String car = "Mustang";
+        String color = "blue";
+
+        System.out.println(name);
+    }
+}
