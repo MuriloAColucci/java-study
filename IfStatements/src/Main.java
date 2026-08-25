cfb69b8ff57f4c279f53a939339afa4d1b7ddda1
murilo <murilocolucci@gmail.com>
2026-08-25 10:26:50 -0300

    if statements

---
+++ IfStatements/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        String name;
+        int age;
+        boolean isStudent;
+
+        System.out.print("Enter your name: ");
+        name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        age = scanner.nextInt();
+
+        System.out.print("You are a student (true/false): ");
+        isStudent = scanner.nextBoolean();
+
+        if(name.isEmpty()){
+            System.out.println("You didn't enter your name! ");
+        } else {
+            System.out.println("Hello " + name + "!");
+        }
+
+
+        if (age >= 65){
+            System.out.println("You are a senior!");
+        } else if(age >= 18){
+            System.out.println("You are an adult!");
+        }  else if (age < 0){
+            System.out.println("You haven't been born yet!");
+        } else if (age == 0){
+            System.out.println("You are a baby!");
+        } else {
+            System.out.println("You are a child!");
+        }
+
+
+        if(isStudent){
+            System.out.println("You are a student");
+        } else {
+            System.out.println("You are NOT a student");
+        }
+    }
+}
