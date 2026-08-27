0a6875d3fc7d350a2c71de90d3863a560086199a
murilo <murilocolucci@gmail.com>
2026-08-27 11:01:08 -0300

    ternary oparator

---
+++ Fundamentals/TernaryOperator/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        int score = 70;
+//        String passOrFail = (score >= 60) ? "PASS" : "FAIL";
+
+//        int number = 3;
+//        String evenOrOdd = (number % 2 == 0) ? "EVEN" : "ODD";
+
+//        int hours = 13;
+//        String timeOfDay = (hours < 12) ? "A.M." : "P.M.";
+
+        int income = 39000;
+        double taxRate = (income >= 40000) ? 0.25 : 0.15;
+
+        System.out.println(taxRate);
+    }
+}
