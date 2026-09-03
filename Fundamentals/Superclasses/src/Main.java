2ee67f49cae5ad4cc60b87e5c03926381f37b3ff
MuriloAColucci <murilocolucci@gmail.com>
2026-09-03 15:01:07 -0300

    super key word in classes

---
+++ Fundamentals/Superclasses/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Person person = new Person("Tom", "Riddle");
+        Student student = new Student("Harry", "Potter", 3.25);
+        Employee employee = new Employee("Rubeus", "Hagrid", 50000);
+
+        student.showGPA();
+        employee.showSalary();
+    }
+}
