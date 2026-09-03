3be6dea51dc295271e336fb757135478ffaf9107
MuriloAColucci <murilocolucci@gmail.com>
2026-09-03 11:40:02 -0300

    array of objects

---
+++ Fundamentals/ArrayObjects/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Car[] cars = {
+                new Car("Mustang", "Red"),
+                new Car("Corvette", "Blue"),
+                new Car("Charger", "Yellow")
+        };
+
+        for (Car car : cars){
+            car.color = "black";
+        }
+
+        for (Car car : cars){
+            car.drive();
+        }
+    }
+}
