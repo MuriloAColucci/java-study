536b354e4bc13f731f703ef9c058a457d800e6d3
MuriloAColucci <murilocolucci@gmail.com>
2026-09-03 10:04:47 -0300

    object oriented programming

---
+++ Fundamentals/ObjectOriented/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+//        System.out.println(car1.make);
+//        System.out.println(car1.model);
+//        System.out.println(car1.year);
+//        System.out.println(car1.price);
+
+//        System.out.println(car1.isRunning);
+//        car1.start();
+//        System.out.println(car1.isRunning);
+//        car1.stop();
+//        System.out.println(car1.isRunning);
+
+        car1.drive();
+        car1.brake();
+
+    }
+}
