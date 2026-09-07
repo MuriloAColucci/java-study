1e18558d927aa857037ab6ea58d4f59a4b15de65
MuriloAColucci <murilocolucci@gmail.com>
2026-09-07 10:06:39 -0300

    hashmaps

---
+++ Fundamentals/HashMaps/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+        HashMap<String, Double> map = new HashMap<>();
+
+        map.put("apple", 0.50);
+        map.put("orange", 0.75);
+        map.put("banana", 0.25);
+        map.put("coconut", 1.00);
+
+        //map.remove("apple");
+        //System.out.println(map.get("apple"));
+        //System.out.println(map.containsKey("banana"));
+
+//        if(map.containsKey("pineapple")){
+//            System.out.println(map.get("apple"));
+//        } else {
+//            System.out.println("Key not found!");
+//        }
+
+        //System.out.println(map.containsValue(1.00));
+
+        //System.out.println(map.size());
+
+        for(String key : map.keySet()){
+            System.out.println(key + " : $" + map.get(key));
+        }
+    }
+}
